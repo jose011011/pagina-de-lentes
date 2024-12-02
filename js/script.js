298546c510b287ac277e6851de097d58e9a1559b
jose011011 <juanmamani121432@gmail.com>
2024-12-02 15:34:08 -0400

    Add files via upload
    pagina de lentes con boostrap

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Menu toggle para móviles
+document.getElementById('menuToggle').addEventListener('click', function () {
+    const nav = document.querySelector('header nav');
+    nav.classList.toggle('open');
+  });
+  
